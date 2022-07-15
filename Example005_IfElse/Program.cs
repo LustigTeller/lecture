14fc4facd3b36b065df9185a243e2a10e2c1782f
«roma <teterdynko_roman@mail.ru>
2022-07-16 00:23:24 +0300

    ex005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if ( username.ToLower() == "рома")
+{
+    Console.WriteLine("Привет, РОМА!!");
+}
+else
+{
+    Console.WriteLine($"Привет, {username}");
+}
