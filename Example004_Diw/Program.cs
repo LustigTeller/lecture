e75079936ae38fb9a91a6bd700951ea35703840e
«roma <teterdynko_roman@mail.ru>
2022-07-16 00:06:25 +0300

    ex004_Diw + newRandom

--- Example004_Diw/Program.cs
+++ Example004_Diw/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 11;
-double numberB = 3.3;
+﻿double numberA = new Random().Next(1, 10); //1,2,3,......9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
