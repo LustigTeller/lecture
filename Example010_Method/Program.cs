72688a1c2d5d82f834c6b05c8851cc9392bdf917
«roma <teterdynko_roman@mail.ru>
2022-07-17 17:08:41 +0300

    ex010_Method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 3, 4, 5, 6, 7, 2, 3, 5, 4 };
+//              0  1  2  3  4  5  6  7  8
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
